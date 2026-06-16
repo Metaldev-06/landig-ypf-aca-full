5c8afb009c6e726113e5745c910ce9b9d27152ae
Metaldev-06 <fernydiaz62@gmail.com>
2026-06-16 19:37:11 -0300

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.diazserviciosypf.com.ar',
+  compressHTML: true,
+  integrations: [sitemap()],
+  prefetch: {
+    defaultStrategy: 'hover',
+  },
 });
